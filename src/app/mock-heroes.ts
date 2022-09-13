c24924803602bb8e91aece61734d210c7768aca0
Sourabh Singh <sourabhsingh19122001@gmail.com>
2022-09-13 18:09:18 +0530

    Angular App

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES:Hero[]=[
+    { id: 1, name: 'Perman' },
+    { id: 2, name: 'Doraemon'},
+    { id: 3, name: 'Superman'},
+    { id: 4, name: 'Hulk'},
+    { id: 5, name: 'Goku'},
+    { id: 6, name: 'Pokemon'},
+    { id: 7, name: 'Kikretsu'},
+    { id: 8, name: 'Venom'},
+    { id: 9, name: 'Sinchan'},
+    { id: 10, name: 'Ben 10'},
+]
